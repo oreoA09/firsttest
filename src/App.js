c140c05447c781905342d995bbc73e4ad56f5e45
oreoA09 <oreo.akanni@gmail.com>
2022-12-23 12:52:57 +0000

    unitTesting header textContent & btn onClick event

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [btnColor, setBtnColor] = useState("green");
+  const newBtnColor = btnColor === "green" ? "blue" : "green";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          going home
-        </a>
-      </header>
+      <h1>Test Website</h1>
+
+      <button style={{backgroundColor: btnColor, color: 'white'}} onClick = {() => setBtnColor(newBtnColor)}>
+        Change Color
+      </button>
+      {/* <header className="App-header"> */}
+      {/* <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React going home
+      </a> */}
+      {/* </header> */}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
 
-test('renders learn react link', () => {
+
+test('the header renders with the correct text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/going home/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerEl = screen.getByRole('heading');
+  expect(headerEl.textContent).toBe('Test Website');
 });
+
+test('button changes color when clicked', () => {
+  render(<App />);
+  const colorBtn = screen.getByRole('button');
+
+  userEvent.click(colorBtn);
+
+  expect(colorBtn).toHaveStyle({backgroundColor: 'blue'})
+  
+  expect(colorBtn.textContent).toBe('Change Color');
+});
+
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/going home/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
